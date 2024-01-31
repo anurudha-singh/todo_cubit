94dab2d12ede49243a38e1d9f93fabdf6f4163a9
Lucifer <singhanurudh570@gmail.com>
2024-01-31 18:18:23 +0530

    cards added

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -21,7 +21,26 @@ class TodoList extends StatelessWidget {
               final todo = todos[index];
 
               return ListTile(
-                title: Text(todo.name),
+                title: Column(
+                  children: [
+                    Card(
+                        margin: EdgeInsets.all(5),
+                        shadowColor: Colors.black,
+                        elevation: 10,
+                        child: Container(
+                            color: Color.fromARGB(255, 123, 236, 202),
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            child: Text(
+                              todo.name,
+                              style: TextStyle(fontSize: 25),
+                            ))),
+                    Divider()
+                  ],
+                ),
+                trailing: Checkbox(
+                  value: false,
+                  onChanged: (bool? value) {},
+                ),
               );
             },
           );
