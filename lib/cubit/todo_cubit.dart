a044a1a27293c64aa17f75f3980f25d322b7d4b6
Lucifer <singhanurudh570@gmail.com>
2024-01-31 14:33:55 +0530

    comments added in cubit class for onject creations

--- lib/cubit/todo_cubit.dart
+++ lib/cubit/todo_cubit.dart
@@ -13,7 +13,9 @@ class TodoCubit extends Cubit<List<Todo>> {
       name: title,
       createdAt: DateTime.now(),
     );
-
+    //List<ToDo> todo=[todo_obj1,todo_obj2,todo_obj3,...., new todo_objn,]
+    // state.add(todo);
+    // this line will cause problem becuase the state will be treated as same it before and it will not emit any new state towards the UI becuase cubit emits any state only when there is change in a state..
     emit([...state, todo]);
   }
 
